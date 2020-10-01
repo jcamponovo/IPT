550480d406c554263bb79ce9d06c6c756a2fa50e
jcamponovo <50249274+jcamponovo@users.noreply.github.com>
2020-10-01 06:35:46 +0200

    Add files via upload

--- masquer.py
+++ masquer.py
@@ -1,7 +1,7 @@
 from IPython.display import HTML 
 import random
 
-def cacher_code(text = "Montrer/Cacher le code",for_next=True):
+def cacher_code(text = "Montrer/Cacher le code",for_next=True,output=False,for_markdown=False):
     this_cell = """$('div.cell.code_cell.rendered.selected')"""
     next_cell = this_cell + '.next()'
     toggle_text = text  # text shown on toggle link
@@ -15,7 +15,43 @@ def cacher_code(text = "Montrer/Cacher le code",for_next=True):
 
     js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
 
-    html = """
+    if output:
+        html = """
+    <script>
+        function {f_name}() {{
+            {cell_selector}.find('div.input').toggle();
+            {cell_selector}.find('div.output').toggle();
+        }}
+
+        {js_hide_current}
+    </script>
+
+    <a href="javascript:{f_name}()">{toggle_text}</a>
+    """.format(
+    f_name=js_f_name,
+    cell_selector=target_cell,
+    js_hide_current=js_hide_current, 
+    toggle_text=toggle_text
+    )
+    elif for_markdown:
+        html = """
+    <script>
+        function {f_name}() {{
+            {cell_selector}.find('div.inner_cell').toggle();
+        }}
+
+        {js_hide_current}
+    </script>
+
+    <a href="javascript:{f_name}()">{toggle_text}</a>
+    """.format(
+    f_name=js_f_name,
+    cell_selector=target_cell,
+    js_hide_current=js_hide_current, 
+    toggle_text=toggle_text
+    )
+    else:
+        html = """
     <script>
         function {f_name}() {{
             {cell_selector}.find('div.input').toggle();
